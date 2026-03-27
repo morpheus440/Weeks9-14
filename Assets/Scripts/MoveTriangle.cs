457284ec7d7ca1145e012bd25b2580746d7af606
pincurow <pincurow@OA-S142-03>
2026-03-27 10:44:06 -0400

    Reworked the animation curve lerp line.

--- Assets/Scripts/MoveTriangle.cs
+++ Assets/Scripts/MoveTriangle.cs
@@ -1,27 +1,29 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveTriangle : MonoBehaviour
 {
     public AnimationCurve curve;
     public Transform target;
+    public float t = 0f;
     public float duration = 5f;
     Vector3 startPosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        startPosition = transform.position;
+
     }
 
     void Update()
     {
-        startPosition = transform.position;
-        float time = Time.deltaTime;
-        float temp = 0f;
-        while(temp < duration)
-        {
-            temp += time;
-            Vector3 position = Vector3.Lerp(startPosition, target.position, time);
-            transform.position = position + new Vector3(0f, curve.Evaluate(temp / duration), 0f);
-        }
+        t += Time.deltaTime;
+        transform.position = Vector3.Lerp(startPosition, target.position, t / duration);
+
+        float temp = curve.Evaluate(t);
+
+        Vector3 position = transform.position;
+        position.y += temp - 0.5f;
+        transform.position = position;
     }
 }
